953d74dba69ee5a605be6ad349c55792c3b7e1e2
Takashi Nishina <tnishina@Takashis-MacBook-Air-2020.local>
2021-01-03 02:32:41 +0900

    modify sum_args.js

--- hello_node/ex02/sum_args.js
+++ hello_node/ex02/sum_args.js
@@ -6,7 +6,7 @@
 /*   By: tnishina <tnishina@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/31 20:12:45 by tnishina          #+#    #+#             */
-/*   Updated: 2021/01/02 15:36:15 by tnishina         ###   ########.fr       */
+/*   Updated: 2021/01/03 02:31:59 by tnishina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ if (process.argv.length <= i) {
 	console.log("Error: provide at least one number");
 } else {
 	while (i < num_str.length)
-	sum += parseInt(num_str[i++], 10);
+		sum += parseInt(num_str[i++], 10);
 	if (sum === sum) {
 		console.log(sum);
 	} else {
